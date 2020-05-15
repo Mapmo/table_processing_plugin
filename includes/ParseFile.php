0dedbf8fe06deec6134c5ee3216f2f2c97bbefdb
Martin Iliev <mapmoeu@gmail.com>
2020-05-16 01:39:38 +0300

    Fix indentation

--- includes/ParseFile.php
+++ includes/ParseFile.php
@@ -1,28 +1,28 @@
 <?php
-$targetDir = "uploads/";
-if (!file_exists($targetDir)) {
-    mkdir($targetDir, 0777, true);
-}
-
-// filter empty file names and paths
-$files = array_filter($_FILES['upload']['name']);
-$total = count($_FILES['upload']['name']);
-
-for ($i = 0; $i < $total; $i++) {
-
-    $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
-
-    if ($tmpFilePath != "") {
-
-	$fileName = $_FILES['upload']['name'][$i];
-	$newFileName = (microtime(true)) . "-" . $fileName;
-	$newFilePath = $targetDir . $newFileName;
-
-        if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-		//if (! $xlsx[$newFileName] = SimpleXLSX::parse($newFilePath)){
-		//	die("Error parsing file $newFilePath");
-		//}
-		$_SESSION[$fileName] = $newFilePath;
+	$targetDir = "uploads/";
+	if (!file_exists($targetDir)) {
+	    mkdir($targetDir, 0777, true);
 	}
-    }
-}
+
+	// filter empty file names and paths
+	$files = array_filter($_FILES['upload']['name']);
+	$total = count($_FILES['upload']['name']);
+
+	for ($i = 0; $i < $total; $i++) {
+
+	    $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+
+	    if ($tmpFilePath != "") {
+
+		$fileName = $_FILES['upload']['name'][$i];
+		$newFileName = (microtime(true)) . "-" . $fileName;
+		$newFilePath = $targetDir . $newFileName;
+
+		if (move_uploaded_file($tmpFilePath, $newFilePath)) {
+			//if (! $xlsx[$newFileName] = SimpleXLSX::parse($newFilePath)){
+			//	die("Error parsing file $newFilePath");
+			//}
+			$_SESSION[$fileName] = $newFilePath;
+		}
+	    }
+	}
