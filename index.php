d2b92d00a5c1768f038b3edadf116568cba90d1a
Martin Iliev <mapmoeu@gmail.com>
2020-05-16 01:37:39 +0300

    Reset session when user attempts to upload new files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+	<title>Table Processing Plugin</title>
+</head>
+
+<body>
+	<?php
+		session_start();
+		session_unset();
+		session_destroy();
+	?>
+    	<form action="visual.php" enctype="multipart/form-data" method="post">
+		<label for="upload">Select files (accepts only Excel 2007+ files):</label>
+		<input type="file" id="upload" name="upload[]" accept=".xlsx" multiple="multiple"><br><br>
+		<input type="submit">
+    	</form>
+</body>
+
+</html>
