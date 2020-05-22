0186baebb12693e4f602d73e9a7dce4eb6dbf6b6
Tanya Hristova <tanche98@gmail.com>
2020-05-22 16:04:12 +0300

    apply PascalCase for ToAlpha function

--- update_table.php
+++ update_table.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-function toAlpha($num)
+function ToAlpha($num)
 {
     return chr(substr("000" . ($num + 65), -3));
 }
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     for ($i = 1; $i <= $cntRow; $i++) {
         for ($j = 1; $j <= $cntCol; $j++) {
-            $worksheet->getCell(toAlpha($j - 1) . $i)->setValue($_POST[$i . "|" . $j]);
+            $worksheet->getCell(ToAlpha($j - 1) . $i)->setValue($_POST[$i . "|" . $j]);
         }
     }
     $writer = new Xlsx($spreadsheet);
