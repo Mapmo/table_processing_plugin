df646bc196ea562b1b653c52e345ebb50db75740
Tanya Hristova <tanche98@gmail.com>
2020-05-22 16:02:22 +0300

    apply PascalCase for ReadfileChunked function

--- streamfile.php
+++ streamfile.php
@@ -3,7 +3,7 @@ define('CHUNK_SIZE', 1024 * 1024); // Size (in bytes) of tiles chunk
 $mimetypes = ["xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"];
 
 // Read a file and display its content chunk by chunk
-function readfile_chunked($filename)
+function ReadfileChunked($filename)
 {
     $fileHandler = fopen($filename, 'rb');
 
@@ -47,7 +47,7 @@ if (empty($exportExtension) || empty($exportFilename)) {
 header('Content-Type: ' . $mimetypes[$exportExtension]);
 header('Content-Disposition: attachment; filename=' . basename($exportFilename . "." . $exportExtension));
 
-if (!readfile_chunked($fileToSave)) {
+if (!ReadfileChunked($fileToSave)) {
     error_log("Problem during streaming file [" . $fileToSave . "] to the client");
     echo "<p>Problem occurred during the file download. Please try again</p>";
 }
