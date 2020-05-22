c59750ec47aadc4bfa9ca6787060905ae708275e
Tanya Hristova <tanche98@gmail.com>
2020-05-22 14:21:51 +0300

    add check function call on submit of second form

--- visual.php
+++ visual.php
@@ -15,29 +15,26 @@ echo '</select>';
 echo '<input type="submit"/>';
 echo '</form>';
 
-if(!isset($_GET['table'])){
+if (!isset($_GET['table'])) {
     exit;
 }
 
 include "includes/PrintTable.php";
-define("SUPPORTED_FILE_EXTENSIONS",["xlsx"]);
+define("SUPPORTED_FILE_EXTENSIONS", ["xlsx"]);
 
 $uploadedFileName = $_GET['table'];
-$nameOfTable = pathinfo(explode('-',$uploadedFileName)[1],PATHINFO_FILENAME);
+$nameOfTable = pathinfo(explode('-', $uploadedFileName)[1], PATHINFO_FILENAME);
 
-echo '<form action="streamfile.php">';
+echo '<form action="streamfile.php" onsubmit="return check()">';
 echo '<label for="table">Choose a name for the exported file:</label>';
-echo '<input type="text" name="file_to_save" value="'.$uploadedFileName.'" hidden>';
-echo '<input type="text" name="export_filename" value="'.$nameOfTable.'" required>';
+echo '<input type="text" name="file_to_save" value="' . $uploadedFileName . '" hidden>';
+echo '<input type="text" name="export_filename" value="' . $nameOfTable . '" required>';
 echo '<select id="extension" name="export_file_extension" required>';
 foreach (SUPPORTED_FILE_EXTENSIONS as $ext) {
-    echo '<option value="' .$ext .'">.'.$ext.'</option>';
+    echo '<option value="' . $ext . '">.' . $ext . '</option>';
 }
 echo '</select>';
 echo '<input type="submit" value="Save as">';
 echo '</form>';
 
 exit;
-
-
-
