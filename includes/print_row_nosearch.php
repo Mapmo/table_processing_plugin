b1fab455b1de953c7bbff3c7891f64c1c6328503
Martin Iliev <mapmoeu@gmail.com>
2020-05-25 16:27:59 +0300

    Add cases for printing rows based on whether the user is searching for something

---
+++ includes/print_row_nosearch.php
@@ -0,0 +1,10 @@
+<?php  
+  foreach ($xlsx->rows() as $r) {
+        $row++;
+        echo '<tr>';
+        for ($i = 0; $i < $column; $i++) {
+            echo '<td><input name="' . $row . '|' . ($i + 1) . '" type="text" value="' . $r[$i] . '"/></td>';
+        }
+        echo '</tr>';
+    }
+?>
