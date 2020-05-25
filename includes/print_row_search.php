8062c0d632d84e80b6e65ddc974114c6ca0b7745
Martin Iliev <mapmoeu@gmail.com>
2020-05-25 17:20:31 +0300

    Add search functionality

--- includes/print_row_search.php
+++ includes/print_row_search.php
@@ -1,10 +1,30 @@
 <?php  
-  foreach ($xlsx->rows() as $r) {
+$hasAnyItem = false; #in case there is no match
+
+foreach ($xlsx->rows() as $r) {
         $row++;
-        echo '<tr>';
+	
+	$item = $_GET['search'];
+	$hasItem = false;
         for ($i = 0; $i < $column; $i++) {
-            echo '<td><input name="' . $row . '|' . ($i + 1) . '" type="text" value="' . $r[$i] . '"/></td>';
-        }
-        echo '</tr>';
-    }
+           	if ($r[$i] === $item) {
+			$hasItem = true;
+			$hasAnyItem = true;
+			break;
+        	}
+
+		if ($hasItem === true) {
+			echo '<tr>';
+			for ($i = 0; $i < $column; $i++) {
+			    echo '<td><input name="' . $row . '|' . ($i + 1) . '" type="text" value="' . $r[$i] . '"/></td>';
+			}
+			echo '</tr>';
+		}
+	}
+}	
+
+if($hasAnyItem === false) {
+	echo '<tr><td><h1>No match found</h1></td></tr>';
+}
+
 ?>
