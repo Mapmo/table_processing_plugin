2194366c17857115df66fbc006aa0121a826f4c8
Martin Iliev <mapmoeu@gmail.com>
2020-05-25 18:04:53 +0300

    Fix an issue with the search utility caused by cases

--- includes/print_row_search.php
+++ includes/print_row_search.php
@@ -4,24 +4,24 @@ $hasAnyItem = false; #in case there is no match
 foreach ($xlsx->rows() as $r) {
         $row++;
 	
-	$item = $_GET['search'];
+	$item ='/' . $_GET['search'] . '/i';
 	$hasItem = false;
         for ($i = 0; $i < $column; $i++) {
-           	if ($r[$i] === $item) {
+		if (preg_match($item, $r[$i])) {
 			$hasItem = true;
 			$hasAnyItem = true;
 			break;
         	}
-
-		if ($hasItem === true) {
-			echo '<tr>';
-			for ($i = 0; $i < $column; $i++) {
-			    echo '<td><input name="' . $row . '|' . ($i + 1) . '" type="text" value="' . $r[$i] . '"/></td>';
-			}
-			echo '</tr>';
-		}
 	}
-}	
+
+	if ($hasItem === true) {
+		echo '<tr>';
+		for ($i = 0; $i < $column; $i++) {
+		    echo '<td><input name="' . $row . '|' . ($i + 1) . '" type="text" value="' . $r[$i] . '"/></td>';
+		}
+			echo '</tr>';
+	}
+}
 
 if($hasAnyItem === false) {
 	echo '<tr><td><h1>No match found</h1></td></tr>';
