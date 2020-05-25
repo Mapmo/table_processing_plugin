6914c7463e490cbf56e46a61f53bed823fb8bf95
Martin Iliev <mapmoeu@gmail.com>
2020-05-25 23:35:39 +0300

    Optimize the code in print_row_search.php by removing the hidden input attribute

--- includes/print_row_search.php
+++ includes/print_row_search.php
@@ -5,11 +5,9 @@ foreach ($xlsx->rows() as $r) {
         $row++;
 	
 	$item ='/' . $_GET['search'] . '/i';
-	$height = 'style="display: none;"';
-   	$hidden = "hidden";
+	$height = 'style="display: none;"'; //if the regex is not met, then make the line invisible
         for ($i = 0; $i < $column; $i++) {
 		if (preg_match($item, $r[$i])) {
-			$hidden = "";
 			$height = "";
 			$hasAnyItem = true;
 			break;
@@ -18,7 +16,7 @@ foreach ($xlsx->rows() as $r) {
 
 	echo '<tr ' . $height . '>';
 	for ($i = 0; $i < $column; $i++) {
-	    echo '<td><input name="' . $row . '|' . ($i + 1) . '" type="text" value="' . $r[$i] . '" ' . $hidden . '/></td>';
+	    echo '<td><input name="' . $row . '|' . ($i + 1) . '" type="text" value="' . $r[$i] . '"/></td>';
 	}
 	echo '</tr>';
 }
