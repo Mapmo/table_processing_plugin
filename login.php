5c05fce1673b92a1cb3c32925419e691c9600484
Martin Iliev <mapmoeu@gmail.com>
2020-05-31 21:23:05 +0300

    Add login validation of input

--- login.php
+++ login.php
@@ -2,8 +2,18 @@
 <html>
 <head>
         <meta charset="utf-8" />
+	<link rel="stylesheet" href="includes/css/main.css">
 </head>
 <body>
+<?php
+if (isset($_GET['warn'])) {
+	switch ($_GET['warn']) {
+		case 'captcha':
+			echo '<h2 class="warn">Wrong captcha</h2>';
+			break;
+	}
+}
+?>
         <form action="includes/login.php" method="POST">
                 User: <input type="text" name="user" /><br />
                 Password: <input type="password" name="pass" /><br />
