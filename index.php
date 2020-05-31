5f1c8730b6d793087873a34bf6279565fbd8b454
Martin Iliev <mapmoeu@gmail.com>
2020-05-31 19:39:33 +0300

    Add functionality that allows only logged users to upload tables to the system

--- index.php
+++ index.php
@@ -7,11 +7,18 @@
 </head>
 
 <body>
-	<form action="visual.php" enctype="multipart/form-data" method="post">
-		<label for="upload">Select files (accepts only Excel 2007+ files):</label>
-		<input type="file" id="upload" name="upload[]" accept=".xlsx" multiple="multiple"><br><br>
-		<input type="submit">
-	</form>
+	<?php if(isset($_SESSION['user'])) { ?>
+		<form action="visual.php" enctype="multipart/form-data" method="post">
+			<label for="upload">Select files (accepts only Excel 2007+ files):</label>
+			<input type="file" id="upload" name="upload[]" accept=".xlsx" multiple="multiple"><br><br>
+			<input type="submit">
+		</form>
+
+	<?php } else { ?>
+	<h1>You are currently not logged in the system.</h1>
+	<a href="login.php">Login</a>
+	<a href="register.php">Register</a>
+	<?php } ?>
 </body>
 
 </html>
