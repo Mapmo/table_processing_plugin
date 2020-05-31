fcce69b9013cd55c7eeec72779e9e7c4543c25ec
Martin Iliev <mapmoeu@gmail.com>
2020-05-31 23:07:36 +0300

    Create the user home directories functionality

--- includes/register.php
+++ includes/register.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include "utils/form_validation.php";
+include "utils/utils.php";
 
 if(ValidateCaptcha() === false) {
        	header('Location: /register.php?warn=captcha');
@@ -13,7 +14,15 @@ if(ValidatePasswordRetype() === false) {
 include "db_connection.php";
 $db_connection = OpenCon();
 $login_query = "INSERT into users (user, password) VALUES ('" . $_POST['user'] . "', '" .  $_POST['pass'] . "')";
-$login=mysqli_query($db_connection, $login_query) || die(mysqli_error($db_connection));
+$login = mysqli_query($db_connection, $login_query) || die(mysqli_error($db_connection));
+
+$get_id_query = "SELECT id from users WHERE user = '" . $_POST['user'] . "'";
+$id = mysqli_query($db_connection, $get_id_query) || die(mysqli_error($db_connection));
+
+$skel="../users/0";
+$home="../users/" . $id;
+
+RecursiveCopy($skel, $home);
 
 CloseCon($db_connection);
 ?>
--- includes/utils/utils.php
+++ includes/utils/utils.php
@@ -8,3 +8,20 @@ function SanitizeInput($input)
 
     return $input;
 }
+
+function RecursiveCopy($src,$dst) { 
+    $dir = opendir($src); 
+    @mkdir($dst); 
+	
+    while(false !== ( $file = readdir($dir)) ) { 
+        if (( $file != '.' ) && ( $file != '..' )) { 
+            if ( is_dir($src . '/' . $file) ) { 
+                RecursiveCopy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+            else { 
+                copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+        } 
+    } 
+    closedir($dir); 
+}
