52d5cd8cc832589836481948ddde575d2d22d7a0
Martin Iliev <mapmoeu@gmail.com>
2020-05-31 21:33:37 +0300

    Improve registration validation

--- register.php
+++ register.php
@@ -2,8 +2,21 @@
 <html>
 <head>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="includes/css/main.css">
 </head>
 <body>
+<?php
+if (isset($_GET['warn'])) {
+        switch ($_GET['warn']) {
+                case 'captcha':
+                        echo '<h2 class="warn">Wrong captcha</h2>';
+                        break;
+		case 'retype':
+			echo '<h2 class="warn">Passwords do not match</h2>';
+			break;
+        }
+}
+?>
         <form action="includes/register.php" method="POST">
                 User: <input type="text" name="user" /><br />
                 Password: <input type="password" name="pass" /><br />
