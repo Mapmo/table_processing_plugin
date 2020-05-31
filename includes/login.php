14e2f24e034a003f2e127ebbb9110b2bcf8a3f50
Martin Iliev <mapmoeu@gmail.com>
2020-06-01 00:45:54 +0300

    Add login in the system functionality

--- includes/login.php
+++ includes/login.php
@@ -4,5 +4,31 @@ include "utils/form_validation.php";
 
 if(ValidateCaptcha()  === false) {
 	header('Location: /login.php?warn=captcha');
+	exit;
 }
+
+include "db_connection.php";
+$db_connection = OpenCon();
+
+#ADD pepper to the user input and hash it
+$user = hash("sha256", $db_connection -> real_escape_string($_POST['user']) . USER_PEPPER);
+$password = hash("sha256", $db_connection -> real_escape_string($_POST['pass']) . PASSWORD_PEPPER);
+
+$login_query = "SELECT id FROM users WHERE user='" . $user . "' AND password='" . $password . "'";
+
+$login = mysqli_query($db_connection, $login_query);
+if (!$login) {
+        die(mysqli_error($db_connection));
+}
+if(mysqli_num_rows($login) === 0) {
+        header('Location: /login.php?warn=data');
+        exit;
+}
+
+#logs the user in the system
+$row = mysqli_fetch_row($login);
+$_SESSION['user']  = $row[0];
+
+CloseCon($db_connection);
+header('Location: /');
 ?>
