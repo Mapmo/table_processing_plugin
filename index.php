c83cd14f718709c6adf781cb06c5992390472079
Martin Iliev <mapmoeu@gmail.com>
2020-06-07 00:50:29 +0300

    Added functionality so that user can select already uploaded tables, that are being parsed by the yaml file for the given user

--- index.php
+++ index.php
@@ -21,8 +21,38 @@
 			<input type="file" id="upload" name="upload[]" accept=".xlsx" multiple="multiple"><br><br>
 			<input type="submit">
 		</form>
-
-	<?php } else { ?>
+		<h2>Select files that you want to edit:</h2>	
+		<?php 
+		
+			#add validation
+			$file = file_get_contents('users/' . $_SESSION['user'] . '/shared_files.yml');
+			$parsed = yaml_parse($file);
+		?>
+		<?php
+			foreach ($parsed as $file) {
+		?>
+				<form action="visual.php" method="post">
+					<input name ="table" value="<?php echo "users/" . $file['owner'] . '/uploads/' . $file['name']; ?>" hidden />
+					<?php 
+					echo $file['owner'] . ': ' . $file['name'] . ' - ';
+						
+					switch($file['write']) {
+					    case 0:
+						 echo "readony";
+						break;
+					    case 1:
+						echo "writeable";
+						break;
+					    default:
+						echo "Corrupted";
+						break;
+					}?>
+					<input type="submit" value="Edit"/>
+				</form>
+				<br/>
+		<?php   } ?>
+		<?php
+		} else { ?>
 	<h1>You are currently not logged in the system.</h1>
 	<a href="login_form.php">Login</a>
 	<a href="register_form.php">Register</a>
