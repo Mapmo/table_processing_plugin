cc2a340075f79b219c2d61c5fc5fc7fe25101fde
Martin Iliev <mapmoeu@gmail.com>
2020-06-08 00:06:22 +0300

    Move the validation whether the user is on top to reduce the big indentation that has happened with if... else statement

--- index.php
+++ index.php
@@ -10,76 +10,75 @@
 <body>
 	<?php
 	session_start();
-	if(isset($_SESSION['user'])) { 
+	if(!isset($_SESSION['user'])) {
+		include("includes/not_logged.php");
+		exit;
+	}
 	?>
-		<!-- Logout -->
-    	<form action="includes/logout.php" onsubmit="return check()">
-        		<input type="submit" value="Logout">
-   		</form>
-
-		<!-- Upload file(s) form -->
-		<?php include("includes/upload_file_form.php"); ?>
-
-		<h2>Files that you have access to:</h2>	
-
-		<!-- The list of accessible files for the user -->
-			<?php
-			#Warning message when the user attemts to do something wrong
-            include("includes/index_warnings.php");
-
-			$filePath  = 'users/' . $_SESSION['user'] . '/shared_files.yml';
-		if (file_exists($filePath)) {
-			$file = file_get_contents('users/' . $_SESSION['user'] . '/shared_files.yml');
-			$parsed = yaml_parse($file);
-
-			if(empty($parsed)) { ?>
-				<h3>You have no uploaded/shared files</h3>
+	<!-- Logout -->
+	<form action="includes/logout.php" onsubmit="return check()">
+			<input type="submit" value="Logout">
+	</form>
+	
+	<!-- Upload file(s) form -->
+	<?php include("includes/upload_file_form.php"); ?>
+	
+	<h2>Files that you have access to:</h2>	
+	
+	<!-- The list of accessible files for the user -->
 		<?php
-			} else {
-				foreach ($parsed as $file) { ?>
-		<!-- The form with the Edit button -->
-					<form action="visual.php" method="post">
-						<input name ="table" value="<?php echo "users/" . $file['owner'] . '/uploads/' . $file['name']; ?>" hidden />
-					<?php 
-					echo $file['owner'] . ': ' . $file['name'] . ' - ';
-						
-					switch($file['write']) {
-					    case 0:
-						 echo "readony";
-						break;
-					    case 1:
-						echo "writeable";
-						break;
-					    default:
-						echo "Corrupted";
-						break;
-					}?>
-					<input type="submit" value="Edit"/>
-				</form>
-
-		<!-- here should be some js that pops the form to type username and rights, rather than having them shown all the time -->
-	    <!-- The form for sharing a file with other users -->
-				<form method="post" action="includes/share_table.php">
-						<input name="name" value="<?php echo $file['name'];?>" hidden />
-						<input name="owner" value="<?php echo $file['owner'];?>" hidden />
-						<input type="text" name="userTo" placeholder="User to share the file with" required />
-						<select name="write">
-							<option value="0">Readonly</option>
-							<option value="1">Edit</option>
-						</select>
-						<input type="submit" value="Share" />
-				</form>
-				<br/>
-		<?php   
-				}
-			}
-		} else { ?>
+		#Warning message when the user attemts to do something wrong
+		include("includes/index_warnings.php");
+	
+		$filePath  = 'users/' . $_SESSION['user'] . '/shared_files.yml';
+	if (file_exists($filePath)) {
+		$file = file_get_contents('users/' . $_SESSION['user'] . '/shared_files.yml');
+		$parsed = yaml_parse($file);
+	
+		if(empty($parsed)) { ?>
 			<h3>You have no uploaded/shared files</h3>
 	<?php
- 		} 
-	} else {
-		include("includes/not_logged.php");
-	} ?>
-</body>
+		} else {
+			foreach ($parsed as $file) { ?>
+	<!-- The form with the Edit button -->
+				<form action="visual.php" method="post">
+					<input name ="table" value="<?php echo "users/" . $file['owner'] . '/uploads/' . $file['name']; ?>" hidden />
+				<?php 
+				echo $file['owner'] . ': ' . $file['name'] . ' - ';
+					
+				switch($file['write']) {
+					case 0:
+				 	echo "readony";
+					break;
+					case 1:
+					echo "writeable";
+					break;
+					default:
+					echo "Corrupted";
+					break;
+				}?>
+				<input type="submit" value="Edit"/>
+			</form>
+	
+	<!-- here should be some js that pops the form to type username and rights, rather than having them shown all the time -->
+	<!-- The form for sharing a file with other users -->
+			<form method="post" action="includes/share_table.php">
+					<input name="name" value="<?php echo $file['name'];?>" hidden />
+					<input name="owner" value="<?php echo $file['owner'];?>" hidden />
+					<input type="text" name="userTo" placeholder="User to share the file with" required />
+					<select name="write">
+						<option value="0">Readonly</option>
+						<option value="1">Edit</option>
+					</select>
+					<input type="submit" value="Share" />
+			</form>
+			<br/>
+	<?php   
+			}
+		}
+	} else { ?>
+		<h3>You have no uploaded/shared files</h3>
+	<?php } ?> 
 
-</html>
+</body>
+</html>	
