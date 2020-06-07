bdb51fe3ef13285697cb34a5b3622325d7df3ba3
Martin Iliev <mapmoeu@gmail.com>
2020-06-07 03:08:08 +0300

    Rename the value of the button for uploading files in order to be more informative

--- index.php
+++ index.php
@@ -20,9 +20,10 @@
 		<form action="includes/parse_file.php" enctype="multipart/form-data" method="post">
 			<label for="upload">Select files (accepts only Excel 2007+ files):</label>
 			<input type="file" id="upload" name="upload[]" accept=".xlsx" multiple="multiple"><br><br>
-			<input type="submit">
+			<input type="submit" value="Upload">
 		</form>
 
+		<!-- The list of accessible files for the user -->
 		<h2>Files that you have access to:</h2>	
 		<?php 
 			$filePath  = 'users/' . $_SESSION['user'] . '/shared_files.yml';
