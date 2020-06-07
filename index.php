36443bd47bac71f56a889e49255d70bf47cfba3d
Martin Iliev <mapmoeu@gmail.com>
2020-06-07 23:45:51 +0300

    Extract the new sharing functionality in a separate code to make the index.php code easier for reading

--- index.php
+++ index.php
@@ -29,17 +29,7 @@
 		<!-- The list of accessible files for the user -->
 			<?php
 			#Warning message when the user attemts to do something wrong
-            if(isset($_GET['warn'])) {
-			 ?>
-                <h3 class="warn">
-            <?php
-                    switch($_GET['warn']) {
-                        case "self_share":
-                            echo "Cannot share with yourself";
-                            break;
-                    }
-                    echo "</h3>";
-			}
+            include("includes/index_warnings.php");
 
 			$filePath  = 'users/' . $_SESSION['user'] . '/shared_files.yml';
 			if (file_exists($filePath)) {
