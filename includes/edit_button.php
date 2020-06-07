5772c0019309ee6bc38e8ed27fcb6d02aff1966b
Martin Iliev <mapmoeu@gmail.com>
2020-06-08 02:51:45 +0300

    Add the readonly functionality for files that were shared with write: 0 permission

--- includes/edit_button.php
+++ includes/edit_button.php
@@ -1,6 +1,7 @@
 <!-- Used in index.php to display a button Edit that leads to the visual.php page, where the user can edit the table -->
 <form action="visual.php" method="post">
 	<input name ="table" value="<?php echo "users/" . $file['owner'] . '/uploads/' . $file['name']; ?>" hidden />
+	<input name ="write" value="<?php echo $file['write']; ?>" hidden />
 	<?php
 	echo $file['owner'] . ': ' . $file['name'] . ' - ';
 
--- includes/print_row_loop.php
+++ includes/print_row_loop.php
@@ -1,9 +1,17 @@
 <?php
+if($_POST['write'] === "1") {
     for ($i = 0; $i < $column; $i++) {
 ?>
         <td><input name="<?php echo $row . '|' . ($i + 1);?>" type="text" value="<?php echo $r[$i] ?>" onclick="updateRowAndCol( <?php echo $row; ?> , <?php echo $i + 1; ?>)" /></td>
 
 <?php
     }
-    echo '</tr>';
+} else {
+	for ($i = 0; $i < $column; $i++) {
+?>
+		<td><?php echo $r[$i]; ?></td>
+<?php    	
+	}
+}
+	echo '</tr>';
 ?>
