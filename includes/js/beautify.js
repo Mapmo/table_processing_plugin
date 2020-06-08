ecc1b1a7e8acc6db77aafa2d693ace4ae08f3636
Tanya Hristova <tanche98@gmail.com>
2020-06-09 01:38:49 +0300

    add functionality to parse the beautifier json file in beautify.js

--- includes/js/beautify.js
+++ includes/js/beautify.js
@@ -1,3 +1,5 @@
+var jsonObj;
+
 function toggleBoldStyle() {
 
     var row = document.getElementById("rowToBeautify").value;
@@ -37,4 +39,32 @@ function toggleUnderlineStyle() {
 function updateRowAndCol(row, col) {
     document.getElementById("rowToBeautify").value = row;
     document.getElementById("colToBeautify").value = col;
+}
+
+function getTableName() {
+    var exportFilename = document.getElementsByName("exportFilename")[0].value;
+    return exportFilename.split('.')[0];
+}
+
+function getUsername() {
+    var fileToSave = document.getElementsByName("fileТoSave")[0].value;
+    return fileToSave.split('/')[1];
+}
+
+function getPathToBeautifier() {
+    return "./users/" + getUsername() + "/beautifiers/" + getTableName() + ".json";
+}
+
+function getJson() {
+
+    var path = getPathToBeautifier();
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.status == 200 && xmlhttp.readyState == 4) {
+            jsonObj = JSON.parse(xmlhttp.responseText);
+        }
+    };
+    xmlhttp.open("GET", path, true);
+    xmlhttp.send();
 }
