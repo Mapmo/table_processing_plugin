2335e745a6f4dfa5b737e488adf6637706b80095
Tanya Hristova <tanche98@gmail.com>
2020-06-09 01:38:09 +0300

    add function onload of page of visual.php

--- visual.php
+++ visual.php
@@ -9,15 +9,15 @@
     <link rel="stylesheet" type="text/css" href="includes/css/beautify.css">
 </head>
 
-<body>
+<body onload="return getJson()">
     <!-- Logout -->
     <form action="includes/logout.php" onsubmit="return check()">
-    	<input type="submit" value="Logout">
+        <input type="submit" value="Logout">
     </form>
 
     <!-- Go Back form -->
-    <form action ="index.php" onsubmit="return check()">
-	<input type="submit" value="Back to home">
+    <form action="index.php" onsubmit="return check()">
+        <input type="submit" value="Back to home">
     </form>
 
     <?php
@@ -30,7 +30,7 @@
     ?>
 
     <!-- Form to choose a phrase to search for in the table -->
-    <form onsubmit="return check()" method="post" >
+    <form onsubmit="return check()" method="post">
         <label for="search">Choose value to search: </label>
         <input type="text" id="search" name="search" value="<?php if (isset($_POST['search'])) {
                                                                 echo $_POST['search'];
@@ -41,7 +41,7 @@
 
     <br>
     <div>
-        <button id="boldButton" onclick="toggleBoldStyle()" ><b>B</b></button>
+        <button id="boldButton" onclick="toggleBoldStyle()"><b>B</b></button>
         <button id="italicButton" onclick="toggleItalicStyle()"><i>I</i></button>
         <button id="underlineButton" onclick="toggleUnderlineStyle()"><u>U</u></button>
         <input id="rowToBeautify" type="hidden">
@@ -53,8 +53,8 @@
     <!-- Form to chooses how to save the table -->
     <form action="includes/streamfile.php" onsubmit="return check()">
         <label for="table">Choose a name for the exported file:</label>
-        <input type="text" name="fileТoSave" value="<?php echo $uploadedFileName ?>" hidden/>
-        <input type="text" name="exportFilename" value="<?php echo basename($uploadedFileName); ?>" required/>
+        <input type="text" name="fileТoSave" value="<?php echo $uploadedFileName ?>" hidden />
+        <input type="text" name="exportFilename" value="<?php echo basename($uploadedFileName); ?>" required />
         <input type="submit" value="Save as">
     </form>
 </body>
