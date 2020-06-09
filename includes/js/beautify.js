2941aef40cc7704f451f4735d170f37c40ff5b88
Tanya Hristova <tanche98@gmail.com>
2020-06-09 17:12:42 +0300

    add functions to handle update of beautifier

--- includes/js/beautify.js
+++ includes/js/beautify.js
@@ -1,4 +1,5 @@
 var jsonObj;
+var pathToBeautifier = "./users/" + getUsername() + "/beautifiers/" + getTableName() + ".json";
 
 function toggleBoldStyle() {
 
@@ -51,20 +52,80 @@ function getUsername() {
     return fileToSave.split('/')[1];
 }
 
-function getPathToBeautifier() {
-    return "./users/" + getUsername() + "/beautifiers/" + getTableName() + ".json";
-}
-
 function getJson() {
-
-    var path = getPathToBeautifier();
-
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.status == 200 && xmlhttp.readyState == 4) {
             jsonObj = JSON.parse(xmlhttp.responseText);
+            applyAllStyles();
         }
     };
-    xmlhttp.open("GET", path, true);
+    xmlhttp.open("GET", pathToBeautifier, true);
     xmlhttp.send();
+}
+
+function applyAllStyles() {
+
+    var boldCells = jsonObj["bold"];
+    var italicCells = jsonObj["italic"];
+    var underlineCells = jsonObj["underline"];
+
+    if (typeof boldCells !== "undefined") {
+        applyStyle(boldCells, "boldStyle");
+    }
+    if (typeof italicCells !== "undefined") {
+        applyStyle(jsonObj["italic"], "italicStyle");
+    }
+    if (typeof underlineCells !== "undefined") {
+        applyStyle(jsonObj["underline"], "underlineStyle");
+    }
+}
+
+function applyStyle(cells, style) {
+    cells.forEach(cell => {
+        var row = cell["row"];
+        var col = cell["col"];
+        var nameOfCell = row + "|" + col;
+        var cell = document.getElementsByName(nameOfCell)[0];
+        cell.classList.add(style);
+    });
+}
+
+function updateBeautifier() {
+    var obj = {
+        "jsonFilePath": pathToBeautifier,
+        "content": {
+            "bold": getUpdatedCells("boldStyle"),
+            "italic": getUpdatedCells("italicStyle"),
+            "underline": getUpdatedCells("underlineStyle"),
+        }
+    };
+
+    var json = JSON.stringify(obj);
+
+    var path = "./includes/beautifier_update.php";
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", path, true);
+
+    xmlhttp.setRequestHeader("Content-Type", "application/json;");
+    xmlhttp.send(json);
+}
+
+function getUpdatedCells(style) {
+    var cells = document.getElementsByClassName(style);
+    var cellsJson = new Array();
+
+    if (cells.length > 0) {
+        Array.from(cells).forEach(cell => {
+            var dimensions = cell["name"].split("|");
+            var dimensionsJson = {
+                "row": dimensions[0],
+                "col": dimensions[1]
+            };
+            cellsJson.push(dimensionsJson);
+        });
+    }
+
+    return cellsJson;
 }
