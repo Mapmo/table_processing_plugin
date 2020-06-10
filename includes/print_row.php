d2af1848ab2284dfc066b4e6314148c723f66ce2
Martin Iliev <mapmoeu@gmail.com>
2020-06-10 23:49:08 +0300

    Fix a bug that was caused by unwillingness to use if else :(. The problem was that by exiting, functiuonalities like the saving and exporting of tables were broken when user was not searching

--- includes/print_row.php
+++ includes/print_row.php
@@ -6,28 +6,27 @@ if (!isset($_POST['search'])) {
 		echo '<tr>';
 		include("includes/print_row_loop.php");
 	}
-	exit;
-}
+} else {
+		foreach ($xlsx->rows() as $r) {
+			$row++;
 
-foreach ($xlsx->rows() as $r) {
-    $row++;
+			$item = '/' . $_POST['search'] . '/i';
+			$height = 'style="display: none;"'; //if the regex is not met, then make the line invisible
+			for ($i = 0; $i < $column; $i++) {
+				if (preg_match($item, $r[$i])) {
+					$height = "";
+					$hasAnyItem = true;
+					break;
+				}
+			}
 
-    $item = '/' . $_POST['search'] . '/i';
-    $height = 'style="display: none;"'; //if the regex is not met, then make the line invisible
-    for ($i = 0; $i < $column; $i++) {
-        if (preg_match($item, $r[$i])) {
-            $height = "";
-            $hasAnyItem = true;
-            break;
-        }
-    }
+			echo '<tr ' . $height . '>';
+			include("includes/print_row_loop.php");
+		}
 
-    echo '<tr ' . $height . '>';
-    include("includes/print_row_loop.php");
-}
-
-if ($hasAnyItem === false) {
-    echo '<tr><td><h1>No match found</h1></td></tr>';
+		if ($hasAnyItem === false) {
+			echo '<tr><td><h1>No match found</h1></td></tr>';
+		}
 }
 ?>
 
