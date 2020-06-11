99bddabc8e8d4ad8ca95199f2a139b59e528a579
Tanya Hristova <tanche98@gmail.com>
2020-06-11 16:26:21 +0300

    Edit absolute path of header's locations to relative because of xampp specifics

--- includes/share_table.php
+++ includes/share_table.php
@@ -3,7 +3,7 @@ session_start();
 $userTo = $_POST['userTo'];
 
 if ($_SESSION['user'] === $userTo) {
-	header('Location: /index.php?warn=self_share');
+	header('Location: ../index.php?warn=self_share');
 	exit;
 }
 
@@ -23,7 +23,7 @@ CloseCon($db_connection);
 $result = $login_query->execute() || die("Failed to query from DB!");
 
 if(!$login_query->fetch(PDO::FETCH_ASSOC)) {
-	header('Location: /index.php?warn=unknown');    
+	header('Location: ../index.php?warn=unknown');    
 	exit;
 }
 
@@ -36,5 +36,5 @@ include("utils/yaml.php");
 
 YamlAppend($yamlPath, $name, $owner, $write);
 
-header('Location: /index.php?ok=share');
+header('Location: ../index.php?ok=share');
 ?>
