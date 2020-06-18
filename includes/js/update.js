762c6723698bd71e852be15ac17b0e6f9a837430
Tanya Hristova <tanche98@gmail.com>
2020-06-18 18:13:11 +0300

    move some function responsible for the update of cells from beautifier.js to update.js

--- includes/js/update.js
+++ includes/js/update.js
@@ -8,6 +8,7 @@ function check() {
         document.getElementById('updateButton').click();
     }
     updateBeautifier();
+    updateLocking();
     sleep(500);
     return true;
 }
@@ -20,7 +21,25 @@ function setFileLockTimeoutSec(seconds) {
     setTimeout(search, seconds*1000);//Countdown till forcefully refreshing the user's session
 }
 
+//called when the body of visual.php is loaded so that any previous styles could be applied and cells - locked
 function getJson() {
     getJsonBeautifier();
     getJsonCellLocking();
+}
+
+// sends an ajax request to update the format - styles and locked cells
+function sendAjaxPostRequest(json) {
+    var path = "./includes/format_update.php";
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", path, true);
+
+    xmlhttp.setRequestHeader("Content-Type", "application/json;");
+    xmlhttp.send(json);
+}
+
+// update hidden input fields in visual.php every time it's clicked on a new cell
+function updateRowAndCol(row, col) {
+    document.getElementById("rowToFormat").value = row;
+    document.getElementById("colToFormat").value = col;
 }
