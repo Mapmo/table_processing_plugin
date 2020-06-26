52f19b5a235c2bd5db4005878ee32338a2524643
Tanya Hristova <tanche98@gmail.com>
2020-06-26 22:49:04 +0300

    Add ok message for deleting a table

--- includes/index_ok.php
+++ includes/index_ok.php
@@ -1,14 +1,17 @@
 <?php
 #Warning message when the user attemts to do something wrong
-if(isset($_GET['ok'])) {
+if (isset($_GET['ok'])) {
 ?>
-<h3 class="ok">
-<?php
-	switch($_GET['ok']) {
+	<h3 class="ok">
+	<?php
+	switch ($_GET['ok']) {
 		case "share":
 			echo "Shared successfully";
+			break;
+		case "delete_table":
+			echo "Table deleted successfully";
 			break;
 	}
 	echo "</h3>";
 }
-?>
+	?>
