5d8c2834e20523b82e9ae9f0b83e8ab333a53d5d
Tanya Hristova <tanche98@gmail.com>
2020-06-26 19:31:11 +0300

    Improve readability in index.php

--- index.php
+++ index.php
@@ -4,32 +4,32 @@
 <head>
 	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
 	<title>Table Processing Plugin</title>
-    <link rel="stylesheet" href="includes/css/main.css">
+	<link rel="stylesheet" href="includes/css/main.css">
 </head>
 
 <body>
 	<?php
-    #Removes the lockfile, thus unlocking the file for editting by others
-    include("includes/unlock_file.php");
+	#Removes the lockfile, thus unlocking the file for editting by others
+	include("includes/unlock_file.php");
 
 	session_start();
 
 	#Validation that the user is logged in the system
-	if(!isset($_SESSION['user'])) {
+	if (!isset($_SESSION['user'])) {
 		include("includes/not_logged.php");
 		exit;
 	}
 	?>
 	<!-- Logout -->
 	<form action="includes/logout.php" onsubmit="return check()">
-			<input type="submit" value="Logout">
+		<input type="submit" value="Logout">
 	</form>
-	
+
 	<!-- Upload file(s) form -->
 	<?php include("includes/upload_file_form.php"); ?>
-	
-	<h2>Files that you have access to:</h2>	
-	
+
+	<h2>Files that you have access to:</h2>
+
 	<!-- The list of accessible files for the user -->
 	<?php
 
@@ -37,10 +37,10 @@
 	include("includes/index_warnings.php");
 	include("includes/index_ok.php");
 	include("includes/utils/yaml.php");
-	
+
 	#Figure the location of the shared_files.yml
 	$filePath  = 'users/' . $_SESSION['user'] . '/shared_files.yml';
-	
+
 	#Validation that the user has his shared_files.yml file
 	if (!file_exists($filePath)) {
 		die("The file that contains information for you shared files is missing. Contact administrators ASAP");
@@ -48,7 +48,7 @@
 	$file = file_get_contents('users/' . $_SESSION['user'] . '/shared_files.yml');
 	$parsed = YamlParse($file);
 
-	if(empty($parsed)) { ?>
+	if (empty($parsed)) { ?>
 		<h3>You have no uploaded/shared files</h3>
 	<?php
 	} else {
@@ -56,14 +56,18 @@
 
 			#The form with the Edit button
 			include("includes/edit_button.php");
-	?>
-		<!-- The form for sharing a file with other users -->
-		<?php include("includes/share_table_form.php"); ?>
-	<br/>
-	<?php   
+
+			#The form for sharing a file with other users
+			include("includes/share_table_form.php");
+
+			#The form for deleting the table
+			include("includes/delete_button.php");
+
+			echo "<br/>";
 		}
 	}
 	?>
 
 </body>
-</html>	
+
+</html>
