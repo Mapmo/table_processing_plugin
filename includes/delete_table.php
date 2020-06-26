3e0df294f4020e66728da4137648606d585e93fd
Tanya Hristova <tanche98@gmail.com>
2020-06-26 22:48:26 +0300

    Implement the deleting of table itself

--- includes/delete_table.php
+++ includes/delete_table.php
@@ -2,14 +2,103 @@
 
 //check if it's locked ---> mutex?
 
-//delete table from yaml 
-// - from owner's  yaml
-// - from the yaml of users the table is shared with
+include("../includes/utils/yaml.php");
 
-//delete yaml for sharing
+$curUser = $_POST['user'];
+$tablePath = $_POST['table'];
+$owner = $_POST['owner'];
 
-//delete beautifiers, locking_cell and coeditor files for the table
+$tableName = pathinfo($tablePath)['filename'];
+$tableNameWithExt = pathinfo($tablePath)['basename'];
 
-//delete the table itself from uploads
+$coeditorsFilePath = "../users/" . $owner . "/coeditors/" . $tableName . ".txt";
 
-?>
+$content = file_get_contents($coeditorsFilePath);
+
+$coeditors = explode("\n", $content);
+
+if ($curUser === $owner) {
+
+    array_push($coeditors, $curUser);
+
+    foreach ($coeditors as $coeditor) {
+
+        if ($coeditor !== "") {
+
+            $sharedPath = "../users/" . $coeditor . "/shared_files.yml";
+            $sharedArr = YamlParse(file_get_contents($sharedPath));
+
+            // remove the table that is being deleted from all coeditors' shared_tables.yml file
+            foreach ($sharedArr as $key => $sharedFileDetails) {
+                if ($sharedFileDetails['name'] === $tableNameWithExt && $sharedFileDetails['owner'] === $owner) {
+                    unset($sharedArr[$key]);
+                }
+            }
+
+            //rewrite the shared_tables.yml file
+            file_put_contents($sharedPath, "");
+
+            foreach ($sharedArr as $sharedFileDetails) {
+                try {
+                    YamlAppend($sharedPath, $sharedFileDetails['name'], $sharedFileDetails['owner'], $sharedFileDetails['write']);
+                } catch (Exception $e) {
+                    header('Location: ../index.php?warn=permissions');
+                    exit;
+                }
+            }
+        }
+    }
+
+    $beautifierPath = "../users/" . $owner . "/beautifiers/" . $tableName . ".json";
+    $cellLockingPath = "../users/" . $owner . "/cell_locking/" . $tableName . ".json";
+
+    //delete beautifiers, locking_cell and coeditor files for the table
+    unlink($beautifierPath);
+    unlink($cellLockingPath);
+    unlink($coeditorsFilePath);
+
+    //delete the table itself from uploads subdirectory
+    unlink("../" . $tablePath);
+
+    header('Location: ../index.php?ok=delete_table');
+} else {
+
+    //remove the coeditor that deletes the table
+    foreach ($coeditors as $key => $coeditor) {
+
+        if ($coeditor === $curUser) {
+            unset($coeditors[$key]);
+        }
+    }
+
+    //rewrite the coeditors file
+    foreach ($coeditors as $coeditor) {
+        if ($coeditor !== "") {
+            YamlAddCoeditor($coeditorsFilePath, $coeditor);
+        }
+    }
+
+    $sharedPath = "../users/" . $curUser . "/shared_files.yml";
+    $sharedArr = YamlParse(file_get_contents($sharedPath));
+
+    // remove the table that is being deleted from current user's shared_tables.yml file
+    foreach ($sharedArr as $key => $sharedFileDetails) {
+        if ($sharedFileDetails['name'] === $tableNameWithExt && $sharedFileDetails['owner'] === $owner) {
+            unset($sharedArr[$key]);
+        }
+    }
+
+    //rewrite the shared_tables.yml file
+    file_put_contents($sharedPath, "");
+
+    foreach ($sharedArr as $sharedFileDetails) {
+        try {
+            YamlAppend($sharedPath, $sharedFileDetails['name'], $sharedFileDetails['owner'], $sharedFileDetails['write']);
+        } catch (Exception $e) {
+            header('Location: ../index.php?warn=permissions');
+            exit;
+        }
+    }
+}
+
+header('Location: ../index.php?ok=delete_table');
