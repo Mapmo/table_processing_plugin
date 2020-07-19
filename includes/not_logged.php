e5d2f4eea1a47ffa262389a130329bb3b9ef7a62
Martin Iliev <mapmoeu@gmail.com>
2020-07-19 23:25:33 +0300

    Fix an issue for google chrome browser that was causing two links to be represented without a tabulation like firefox does. Using a table adds spacing between the Login and Register links

--- includes/not_logged.php
+++ includes/not_logged.php
@@ -1,4 +1,8 @@
 <!-- Included in index.php --> 
 <h1>You are currently not logged in the system.</h1>
-<a href="login_form.php">Login</a>
-<a href="register_form.php">Register</a>
+<table>
+	<tr>
+		<td><a href="login_form.php">Login</a></td>
+		<td><a href="register_form.php">Register</a></td>
+	</tr>
+</table>
